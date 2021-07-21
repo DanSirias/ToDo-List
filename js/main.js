f8d10f1beb037e327dc0ceccc10ddd3cab2e92a3
Daniel Sirias <danielsirias.88@gmail.com>
2021-07-21 23:23:15 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -17,4 +17,31 @@ addToDoButton.addEventListener('click', function(){
         toDoContainer.removeChild(paragraph);
     })
     
-})
+});
+
+
+function PrintList(toDoContainer)
+{
+    var mywindow = window.open('', 'PRINT', 'height=400,width=600');
+    new Date();
+    mywindow.document.write('<html><head><title>' + document.title  + '</title>');
+    mywindow.document.write('</head><body >');
+    mywindow.document.write('<center>');
+    mywindow.document.write(document.getElementById('header').innerHTML);
+    
+    mywindow.document.write('<h1>' + document.title  + '</h1>');
+    mywindow.document.write(document.getElementById('toDoContainer').innerHTML);
+    mywindow.document.write('<p>' + new Date()  + '</p>');
+    mywindow.document.write('</center>');
+    mywindow.document.write('</body></html>');
+
+    mywindow.document.close(); // necessary for IE >= 10
+    mywindow.focus(); // necessary for IE >= 10*/
+
+    mywindow.print();
+    mywindow.close();
+
+    return true;
+};
+
+
