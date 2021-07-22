7b7e13258d56774ceeca003240b08a41475f66ec
[DanSirias] <[danielsirias.88@gmail.com]>
2021-07-22 20:36:28 +0900

    Works-print

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ let inputField = document.getElementById('inputField');
 addToDoButton.addEventListener('click', function(){
     var paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
-    paragraph.innerHTML = inputField.value;
+    paragraph.innerHTML = "[   ]" + "   " + inputField.value;
     toDoContainer.appendChild(paragraph);
     inputField.value = "";
-    
+
     paragraph.addEventListener('click', function(){
         paragraph.style.textDecoration = "line-through";
     })
@@ -27,7 +27,7 @@ function PrintList(toDoContainer)
     mywindow.document.write('<html><head><title>' + document.title  + '</title>');
     mywindow.document.write('</head><body >');
     mywindow.document.write('<center>');
-    mywindow.document.write(document.getElementById('header').innerHTML);
+    mywindow.document.write(document.getElementById('header','background-color=blue').innerHTML);
     
     mywindow.document.write('<h1>' + document.title  + '</h1>');
     mywindow.document.write(document.getElementById('toDoContainer').innerHTML);
@@ -38,8 +38,11 @@ function PrintList(toDoContainer)
     mywindow.document.close(); // necessary for IE >= 10
     mywindow.focus(); // necessary for IE >= 10*/
 
-    mywindow.print();
-    mywindow.close();
+    mywindow.document.write(html);// necessary for Google Chrome
+    mywindow.onload = function () {
+        mywindow.print();
+        mywindow.close();
+    };
 
     return true;
 };
